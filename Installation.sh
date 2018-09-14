ad0d3c42334092775f0165fde5db2d0047e47b34
Ubuntu <ajsharm@ajsharmVM2.5fzbslcmjceeblnxf1rz1s0pge.ix.internal.cloudapp.net>
2018-09-14 12:13:27 +0000

    Modified Installation file

--- Installation.sh
+++ Installation.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/SharmaAjay19/VideoImageColorization.git
 mv /home/ajsharm/VideoImageColorization /home/ajsharm/colorization
-sudo apt install caffe-cpu
-sudo apt install nodejs
-sudo apt install npm
+yes | sudo apt install caffe-cpu
+yes | sudo apt install nodejs
+yes | sudo apt install npm
 cd colorization
 ./models/fetch_release_models.sh
 cd color-server
@@ -10,3 +10,4 @@ mkdir downloads
 mkdir uploads
 npm install
 node index.js
+
